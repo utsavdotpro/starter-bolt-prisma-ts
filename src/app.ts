5549b7846dafdef36108e730c77aab9a5c0fb689
Utsav Barnwal <u.barnwal36@gmail.com>
2023-01-07 16:36:22 +0700

    add slack/bolt

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+const PORT = 3000;
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Starting the app
+  await app.start(process.env.PORT || PORT);
+
+  console.log(`⚡️ Bolt app is running on port ${PORT}!`);
+})();
